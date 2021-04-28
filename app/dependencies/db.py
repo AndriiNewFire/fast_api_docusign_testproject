29025ec0974103474b8663263f8803a359bf3f4d
AndriiNewFire <andrii.divnych@newfireglobal.com>
2021-04-28 17:03:08 +0300

    Adding db integration structure and created SQLAlchemy parts in db.py

---
+++ app/dependencies/db.py
@@ -0,0 +1,13 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+SQLALCHEMY_DATABASE_URL = "postgresql://andrii:password@postgresserver/db"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+session_local = sessionmaker(bind=engine,
+                             autoflush=False,
+                             autocommit=False,
+                             )
+Base = declarative_base()
