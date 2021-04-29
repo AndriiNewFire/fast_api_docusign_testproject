7987f06880a7487fa00094570b62c22847f29595
AndriiNewFire <andrii.divnych@newfireglobal.com>
2021-04-29 13:28:40 +0300

    Adding alembic to the project

---
+++ app/alembic/versions/774c58b35136_create_user_document_table.py
@@ -0,0 +1,24 @@
+"""Create user_document table
+
+Revision ID: 774c58b35136
+Revises: 
+Create Date: 2021-04-29 12:20:57.738279
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '774c58b35136'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
