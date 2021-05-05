c18ee210cabb64141322ec27262819e42808dea4
AndriiNewFire <andrii.divnych@newfireglobal.com>
2021-05-05 17:18:38 +0300

    Working state of /token endpoint that validates the users is DB

--- app/api/users_management/users_api.py
+++ app/api/users_management/users_api.py
@@ -1,122 +1,21 @@
-from datetime import datetime, timedelta
+from datetime import timedelta
 
-from fastapi import Depends, HTTPException
+from fastapi import Depends, HTTPException, status
 from fastapi_utils.cbv import cbv
 from fastapi_utils.inferring_router import InferringRouter
-from fastapi.security import OAuth2PasswordBearer
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 
 from db.user_and_documents_management import crud
+from db.user_and_documents_management.authentication import get_current_user, authenticate_user
+from dependencies.authentication_dependencies import ACCESS_TOKEN_EXPIRE_MINUTES, create_access_token
 from schemes import user_and_documents_schemes
+from schemes.user_and_documents_schemes import User
 from dependencies.db import get_db
 
 
-from typing import Optional
-from passlib.context import CryptContext
-from jose import JWTError, jwt
-from fastapi import Depends, FastAPI, status
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from pydantic import BaseModel
-
-
 router = InferringRouter()
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl='token')
 
-fake_users_db = {
-    "johndoe": {
-        "username": "johndoe",
-        "full_name": "John Doe",
-        "email": "johndoe@example.com",
-        "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
-        "disabled": False,
-    }
-}
-SECRET_KEY = 'a7b9e551df964711519796d9f21d533b79e17958e887f55a94668bc6852e8ea9'
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 300
-
-class User(BaseModel):
-    username: str
-    email: Optional[str] = None
-    full_name: Optional[str] = None
-    disabled: Optional[bool] = None
-
-
-class UserInDB(User):
-    hashed_password: str
-
-
-class Token(BaseModel):
-    access_token: str
-    token_type: str
-
-class TokenData(BaseModel):
-    username: Optional[str] = None
-
-pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
-
-
-def verify_password(plain_password, hashed_password):
-    return pwd_context.verify(plain_password, hashed_password)
-
-def get_password_hash(password):
-    return pwd_context.hash(password)
-
-def authenticate_user(fake_db, username: str, password: str):
-    user = get_user(fake_db,username)
-    if not user or not verify_password(password, user.hashed_password):
-        return False
-    return user
-
-def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
-    to_encode = data.copy()
-    if expires_delta:
-        expire = datetime.utcnow() + expires_delta
-    else:
-        expire = datetime.utcnow() + timedelta(minutes=15)
-    to_encode.update({'exp': expire})
-    excoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    return excoded_jwt
-
-def get_user(db, username: str):
-    if username in db:
-        user_dict = db[username]
-        return UserInDB(**user_dict)
-
-def fake_decode_token(token):
-
-    user = get_user(fake_users_db, token)
-    return user
-
-
-def fake_hash_password(password: str):
-    return "fakehashed" + password
-
-
-async def get_current_user(token: str = Depends(oauth2_scheme)):
-    credentials_exception = HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid authentication credentials",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=ALGORITHM)
-        username: str = payload.get('sub')
-        if username is None:
-            raise credentials_exception
-        token_data = TokenData(username=username)
-    except JWTError:
-        raise credentials_exception
-    user = get_user(fake_users_db, username = token_data.username)
-    if user is None:
-        raise credentials_exception
-    return user
-
-
-async def get_current_active_user(current_user: User = Depends(get_current_user)):
-    if current_user.disabled:
-        raise HTTPException(status_code=400, detail="Inactive user")
-    return current_user
 
 @cbv(router)
 class UserAndDocumentsManagement:
@@ -126,8 +25,9 @@ class UserAndDocumentsManagement:
         return current_user
 
     @router.post("/token")
-    async def login(self, form_data: OAuth2PasswordRequestForm = Depends()):
-        user = authenticate_user(fake_users_db, form_data.username, form_data.password)
+    async def login(self, form_data: OAuth2PasswordRequestForm = Depends(),
+                    db: Session = Depends(get_db)):
+        user = authenticate_user(form_data.username, db)
         if not user:
             raise HTTPException(
                 status_code=status.HTTP_401_UNAUTHORIZED,
@@ -135,15 +35,9 @@ class UserAndDocumentsManagement:
                 headers={"WWW-Authenticate": "Bearer"},
             )
         access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-        access_token = create_access_token(data={'sub': user.username}, expires_delta=access_token_expires)
+        access_token = create_access_token(data={'sub': user.email}, expires_delta=access_token_expires)
 
         return {"access_token": access_token, 'token_type': 'bearer'}
-
-
-
-
-
-
 
     # Endpoint to get all available users
     @router.get('/users/', response_model=list[user_and_documents_schemes.User])
@@ -174,5 +68,3 @@ class UserAndDocumentsManagement:
         if db_user:
             raise HTTPException(status_code=400, detail='Email exists')
         return crud.create_user(db=db, user=user)
-
-
